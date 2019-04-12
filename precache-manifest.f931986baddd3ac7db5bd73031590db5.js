8784ff488051ba8e9963496f5dea6300ab5e5dd8
sappusaketh <sakethkumarkappala@gmail.com>
2019-04-12 18:29:16 -0400

    Updates

---
+++ precache-manifest.f931986baddd3ac7db5bd73031590db5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cbcbeb21d5ad693218e",
+    "url": "/TrelloClone/static/js/runtime~main.1039ce61.js"
+  },
+  {
+    "revision": "ed2e5046524c51394f60",
+    "url": "/TrelloClone/static/js/main.8498f3a7.chunk.js"
+  },
+  {
+    "revision": "f0b521c66bc702a37631",
+    "url": "/TrelloClone/static/js/2.9f4f2337.chunk.js"
+  },
+  {
+    "revision": "ed2e5046524c51394f60",
+    "url": "/TrelloClone/static/css/main.a5a13d4f.chunk.css"
+  },
+  {
+    "revision": "45109b32a521339256bf8dc1452eef77",
+    "url": "/TrelloClone/index.html"
+  }
+];
